75931fb6e904ba4b036878f4d3b72e72388b8d0d
Rasha <rash0081@kea.stud.dk>
2022-02-23 15:06:34 +0100

    commit1

---
+++ src/main/java/com/example/dailypaper/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.example.dailypaper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.stereotype.Controller
+public class Controller {
+
+    @GetMapping("/")
+    public String article(){
+        return "article";
+    }
+}
